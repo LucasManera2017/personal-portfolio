7ecdbf82ec317d4b8f107b5d57292472a6642326
LucasManera2017 <lucas.manera2017@gmail.com>
2025-10-20 13:54:49 -0300

    chore: initialize project setup with navbar and theme controller

---
+++ lucas-portfolio/src/App.tsx
@@ -0,0 +1,14 @@
+import Navbar from "./components/Navbar.js";
+
+function App() {
+
+  return (
+    <>
+      <div className="min-h-screen">
+        <Navbar/>
+      </div>
+    </>
+  );
+}
+
+export default App;
